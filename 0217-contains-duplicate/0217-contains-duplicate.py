a4c0e234b5fbfe998fba1627717f6636c1ff56b0
Ahmed Alsaleh <98479269+ahmedexel@users.noreply.github.com>
2026-07-31 18:22:35 +0200

    Time: 17 ms (40.02%), Space: 32.4 MB (30.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,7 @@
+"""
+We check if the set of nums is a different size to the original nums array, if its not that means there is a duplicate as sets remove duplicates.
+O(n) time O(n) space
+"""
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
